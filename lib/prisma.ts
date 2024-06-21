b9d96a7db7d4a3b00fc553da4106a29c7b82cb0a
jdboachie <jdboachie@gmail.com>
2024-06-21 15:12:09 +0000

    ditching prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from '@prisma/client/edge'
-import { withAccelerate } from '@prisma/extension-accelerate'
 
-// export const prisma = new PrismaClient()
-export const prisma = new PrismaClient().$extends(withAccelerate())
+// not using prisma accelerate any more. testing to see if it's the problem
+// import { withAccelerate } from '@prisma/extension-accelerate'
+
+export const prisma = new PrismaClient()
+// export const prisma = new PrismaClient().$extends(withAccelerate())
